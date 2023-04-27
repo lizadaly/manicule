50993abda66fc27cb9d099b21bea62feda4260c1
Liza Daly <lizadaly@gmail.com>
2023-04-27 15:37:15 -0400

    Close zoomed image in shadow DOM

--- manicule/index.js
+++ manicule/index.js
@@ -1,7 +1,17 @@
-import { CollationModel } from "./collation.js"
-import { StructureView, StructureLeaf } from "./structure.js"
-import { SpreadViewer, LeafNav } from "./spread.js"
-import { NavStrip } from "./navstrip.js"
+import {
+  CollationModel
+} from "./collation.js"
+import {
+  StructureView,
+  StructureLeaf
+} from "./structure.js"
+import {
+  SpreadViewer,
+  LeafNav
+} from "./spread.js"
+import {
+  NavStrip
+} from "./navstrip.js"
 
 customElements.define("collation-model", CollationModel)
 customElements.define("nav-strip", NavStrip)
@@ -12,7 +22,8 @@ customElements.define("structure-leaf", StructureLeaf)
 
 // Add cancel listeners to the parent
 document.addEventListener("click", () => {
-  ;[...document.querySelectorAll(".selected")].map((el) => {
+  ;
+  [...document.querySelectorAll(".selected")].map((el) => {
     el.parentNode.selected = !el.parentNode.selected
     el.classList.toggle("selected")
   })
@@ -20,16 +31,20 @@ document.addEventListener("click", () => {
 
 // ESC resets all zoom options
 document.addEventListener("keydown", (e) => {
+
   switch (e.key) {
     case "Escape": {
-      ;[...document.querySelectorAll("[data-cacheable-image]")].map((el) => {
-        el.zoomed = false
-        el.selected = false
-        el.style.translate = "0px 0px"
-        el.style.transform = "scale(1)"
-        el.classList.remove("selected")
-        el.zIndex = 1
-      })
+      for (const spread of document.querySelectorAll("spread-viewer")) {
+        for (const img of spread.shadowRoot.querySelectorAll('img.selected')) {
+
+          img.zoomed = false
+          img.selected = false
+          img.style.translate = "0px 0px"
+          img.style.transform = "scale(1)"
+          img.classList.remove("selected")
+          img.zIndex = 1
+        }
+      }
     }
   }
 })
